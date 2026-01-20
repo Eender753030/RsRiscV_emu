b1dc8341d9814dbe848930c3f448d3b417cff79d
Eender753030 <ender303030@gmail.com>
2026-01-20 17:23:11 +0800

    feat: Change error to exception for future trap handle

---
+++ crates/riscv-core/src/exception.rs
@@ -0,0 +1,16 @@
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Exception {
+    InstructionAddressMisaligned = 0,
+    InstructionAccessFault = 1,
+    IllegalInstruction = 2,
+    Breakpoint = 3,
+    LoadAddressMisaligned = 4,
+    StoreAddressMisaligned = 6,
+    EnvironmentCallFromMMode = 11,
+}
+
+impl From<Exception> for u8 {
+    fn from(value: Exception) -> Self {
+        value as u8
+    }
+}
