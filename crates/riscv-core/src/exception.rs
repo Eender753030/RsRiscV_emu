7da42d195e77098fa65fd43454dee1f196d4027b
Eender753030 <ender303030@gmail.com>
2026-01-24 19:08:13 +0800

    feat: Add page fault and access fault exceptions

--- crates/riscv-core/src/exception.rs
+++ crates/riscv-core/src/exception.rs
@@ -1,30 +1,38 @@
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Exception {
     InstructionAddressMisaligned,
-    InstructionAccessFault,
+    InstructionAccessFault(u32),
     IllegalInstruction(u32),
     Breakpoint,
     LoadAddressMisaligned,
     LoadAccessFault(u32),
     StoreAddressMisaligned,
+    StoreAccessFault(u32),
     EnvironmentCallFromUMode,
     EnvironmentCallFromSMode,
     EnvironmentCallFromMMode,
+    InstructionPageFault(u32),
+    LoadPageFault(u32),
+    StoreOrAmoPageFault(u32),
 }
 
 impl From<Exception> for u32 {
     fn from(value: Exception) -> Self {
         match value {
             Exception::InstructionAddressMisaligned => 0,
-            Exception::InstructionAccessFault => 1,
+            Exception::InstructionAccessFault(_) => 1,
             Exception::IllegalInstruction(_) => 2,
             Exception::Breakpoint => 3,
             Exception::LoadAddressMisaligned => 4,
             Exception::LoadAccessFault(_) => 5,
             Exception::StoreAddressMisaligned => 6,
+            Exception::StoreAccessFault(_) => 7,
             Exception::EnvironmentCallFromUMode => 8,
             Exception::EnvironmentCallFromSMode => 9,
             Exception::EnvironmentCallFromMMode => 11,
+            Exception::InstructionPageFault(_) => 12,
+            Exception::LoadPageFault(_) => 13,
+            Exception::StoreOrAmoPageFault(_) => 15,
         }
     }
 }
